a6762d9cc986a226beafafc797e46b208ceb3ef0
Ivan Molineris <ivan.molineris@gmail.com>
2021-12-06 19:31:45 +0100

    WIP on multiple levels

--- manuscript_2023_analyses/local/src/ROC.R
+++ manuscript_2023_analyses/local/src/ROC.R
@@ -7,8 +7,9 @@ suppressMessages(library("optparse"))
 options(error = function() traceback(2))
 
 option_list <- list(
-	make_option(c("-O", "--pdfname"), action="store", default="ROC_comparison", dest="pdf_name", help="Specify the name of the pdf image [default: \"ROC_comparison\"]"),
-        make_option(c("-d", "--direction"), action="store", default="auto", dest="direction", help="in which direction to make the comparison? “auto” (default): automatically define in which group the median is higher and take the direction accordingly. “>”: if the predictor values for the control group are higher than the values of the case group (controls > t >= cases). “<”: if the predictor values for the control group are lower or equal than the values of the case group (controls < t <= cases). You should set this explicity to “>” or “<” whenever you are resampling or randomizing the data, otherwise the curves will be biased towards higher AUC values. [default \"%default\"]")
+        make_option(c("-d", "--direction"), action="store", default="auto", dest="direction", help="in which direction to make the comparison? “auto” (default): automatically define in which group the median is higher and take the direction accordingly. “>”: if the predictor values for the control group are higher than the values of the case group (controls > t >= cases). “<”: if the predictor values for the control group are lower or equal than the values of the case group (controls < t <= cases). You should set this explicity to “>” or “<” whenever you are resampling or randomizing the data, otherwise the curves will be biased towards higher AUC values. [default \"%default\"]"),
+	make_option(c("-l", "--level-name"), action="store", default=NULL, dest="level_name", help="Name of the level column used to split the table [deafult:\"None\"]"),
+	make_option(c("-O", "--pdfname"), action="store", default="ROC_comparison", dest="pdf_name", help="Name of the pdf image [default: \"ROC_comparison\"]")
 )
 
 # Esempio per passare stringa con più elementi da trasformare in un vettore di predittori
@@ -53,61 +54,117 @@ outcomes <- shift_fn(args)
 ### The remaining arguments are the predictors
 predictors <- args
 
-### Definition of ROC curve formula including all predictors
-### ROC object
-roc_list <- suppressMessages(roc(as.formula(paste(collapse = "~", c(outcomes, paste(collapse = "+", predictors)))), data = z, direction=opt$direction))
-
 ### Create label strings for the legend
 labels <- predictors
 for(i in seq_along(labels)){
   l<-labels[[i]]
-  auc<-round(roc_list[[l]]$auc,3)
+  auc<-round(roc_obj[[l]]$auc,3)
   labels[[i]]<-paste0(c(l,auc),collapse=": ")
 }
 
-### ROC curve plot
-ROC_plot <- ggroc(roc_list) + 
-      theme_classic(base_size = 10) +
-	    geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed") + 
-	    theme(plot.margin = margin(1,2,1,2, "cm")
-	          , plot.title = element_text(hjust = 0.5)
-	          , legend.title = element_blank()
-	          , legend.position = c(0.75, 0.22)) +
-	    scale_color_discrete(labels=labels) +
-	    scale_linetype_manual(breaks="custom_t_pot",values="dashed") +
-      coord_fixed() 
-# ggtitle("ROC Comparison") +
+### If specified, split dataframe according to "level_name" column
+level_name<-opt$level_name
 
+
+if(!is.null(level_name)){
+	 
+	z_split<-split(z,z$level_name)
+	
+	roc_obj<-lapply(z_split, function(x){
+  		suppressMessages(roc(as.formula(paste(collapse = "~", c(outcomes, paste(collapse = "+", predictors)))), data= x, direction=opt$direction))
+	}
+	
+	table_out <- data.frame(
+		  level1=character()
+		, pred1=character()
+		, AUC1=double()
+		, level2=character()
+		, pred2=character()
+		, AUC2=double()
+		, p_value_cfr=double()
+		, stringsAsFactors=FALSE)
+	
+	### Fill dataframe with AUC comparisons
+	count=1
+	for (l1 in level_name) {
+	  for (p1 in predictors) {
+	    for (l2 in level_name) {
+	      for (p2 in predictors) {
+		if(l1>l2 & p1>p2) {
+		  roc1<-roc_matrix[[l1]][[p1]]
+		  roc2<-roc_matrix[[l2]][[p2]]
+		  table_out[count,]<-c(l1,p1,roc1$auc,l2,p2,roc2$auc,roc.test(roc1,roc2,method="bootstrap")$p.value)
+		}
+	      }
+	    }
+	  }
+	}
+
+
+} else {
+	roc_obj <- suppressMessages(roc(as.formula(paste(collapse = "~", c(outcomes, paste(collapse = "+", predictors)))), data = z, direction=opt$direction))
+	### Create dataframe for stdout print
+	table_out <- data.frame(pred_1=character(),
+			 pred_2=character(),
+			 AUC_1=double(),
+			 AUC_2=double(),
+			 p_value_cfr=double(),
+			 stringsAsFactors=FALSE)
+
+	### Fill each row of the dataframe
+	count=1
+	for(i in predictors){
+	  for(j in predictors){
+	    if(i<j){
+	      roc.pred_1 <- roc(as.formula(paste(outcomes,"~",i)), data= z)
+	      roc.pred_2 <- roc(as.formula(paste(outcomes,"~",j)), data= z)
+	      AUC_1 <- roc.pred_1$auc
+	      AUC_2 <- roc.pred_2$auc
+	      p_value_cfr <- roc.test(roc.pred_1, roc.pred_2)$p.value
+	      table_out[count,] <- c(i,j,AUC_1,AUC_2,p_value_cfr)
+	      count = count + 1
+	    }
+	  }
+	}
+
+}
+
+### ROC curve plot
+if(length(predictors)>1 && !is.null(level_name) && length(level_name)>1){
+
+	ggroc_list<-lapply(1:length(predictors), function(x){
+		  ggroc(lapply(roc_matrix, `[[`, x)) +
+		    theme_classic(base_size = 10) +
+		    geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed") +
+		    theme(plot.margin = margin(1,2,1,2, "cm")
+			  , plot.title = element_text(hjust = 0.5)
+			  , legend.title = element_blank()
+			  , legend.position = c(0.75, 0.22)) +
+		    scale_linetype_manual(breaks="custom_t_pot",values="dashed") +
+		    coord_fixed()
+		})
+
+	ROC_plot<-ggarrange(plotlist=ggroc_list)
+
+} else {
+
+	ROC_plot <- ggroc(roc_obj) + 
+	      theme_classic(base_size = 10) +
+		    geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed") + 
+		    theme(plot.margin = margin(1,2,1,2, "cm")
+			  , plot.title = element_text(hjust = 0.5)
+			  , legend.title = element_blank()
+			  , legend.position = c(0.75, 0.22)) +
+		    scale_color_discrete(labels=labels) +
+		    scale_linetype_manual(breaks="custom_t_pot",values="dashed") +
+	      coord_fixed() 
+	# ggtitle("ROC Comparison") +
+}
 
 ### Get pdf file name from --pdf option
 ### Save plot
 ggsave(paste0(opt$pdf_name,".pdf"), plot=ROC_plot, device="pdf",width = 6,height=6)
 
-### Create dataframe for stdout print
-table_out <- data.frame(pred_1=character(),
-                 pred_2=character(), 
-                 AUC_1=double(), 
-                 AUC_2=double(),
-		 p_value_cfr=double(),	
-		 stringsAsFactors=FALSE) 
-
-### Fill each row of the dataframe
-count=1
-for(i in predictors){
-  for(j in predictors){
-     if(j>i){
-	     roc.pred_1 <- suppressMessages(roc(as.formula(paste(outcomes,"~",i)), data= z, direction=opt$direction))
-	     roc.pred_2 <- suppressMessages(roc(as.formula(paste(outcomes,"~",j)), data= z, direction=opt$direction))
-	     pred_1 <- i
-	     pred_2 <- j
-	     AUC_1 <- roc.pred_1$auc
-	     AUC_2 <- roc.pred_2$auc
-	     p_value_cfr <- roc.test(roc.pred_1, roc.pred_2)$p.value
-	     table_out[count,] <- c(pred_1,pred_2,AUC_1,AUC_2,p_value_cfr)
-	     count = count + 1
-    } 
-  }
-}
 
 ### Save dataframe to stdout
 write.table(table_out, row.names =FALSE, quote = FALSE, file="", sep="\t") # col.names=NA => frist cell empty
