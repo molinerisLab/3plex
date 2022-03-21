6f54d352cbf4cb3451399a821e103684a9998dd0
ccicconetti <chiara.cicconetti@unito.it>
2022-03-21 01:09:10 +0100

    v8.6 negative peaks

--- manuscript_2023_analyses/local/share/rules/prj_rules_v8.6_ReChIRP_idr_overlap_top1000.mk
+++ manuscript_2023_analyses/local/share/rules/prj_rules_v8.6_ReChIRP_idr_overlap_top1000.mk
@@ -7,13 +7,17 @@ ChIRP.bed.split.gz: ../../local/share/data/ReChIRP/idr_overlap_top1000/all_repro
 %_onlypos.bed: ChIRP.bed.split.gz
 	bawk '$$5=="$*" || $$5~/$*.intersec/ {print $$1,$$2,$$3,$$4";"$$5,$$5,"pos"}' $< > $@
 
+# idr + overlap top 1000 are considered positive but all the called peaks are considered in the blacklist
+%_ALLpos.bed: ../../local/share/data/ReChIRP/idr_overlap_top1000/all_reproducibility.idr_conservative-idr_optimal_peak-overalp_conservative-overlap_optimal.regionPeak.gz
+	 bawk '$$1 == "7SK" {print $$2~4,"chirp_peak_" NR";"$$1,$$1,"pos"}' $< > $@
+
 %_neg.bed: ChIRP.bed.split.gz %_onlypos.bed
 	bawk '{print $$1,$$2,$$3,$$4";"$$5,"$*"}' $< > $@.tmp
 	bedtools intersect -a $@.tmp -b $^2 -v \
 	| bawk '{print $$0, "neg" }'> $@
 	rm $@.tmp
 
-%.neg_rand.excl.bed: $(BIOINFO_ROOT)/task/gencode/dataset/hsapiens/32/hg38.shuffle_blacklist.bed $(BIOINFO_ROOT)/task/gencode/dataset/hsapiens/32/gap.bed %_onlypos.bed
+%.neg_rand.excl.bed: $(BIOINFO_ROOT)/task/gencode/dataset/hsapiens/32/hg38.shuffle_blacklist.bed $(BIOINFO_ROOT)/task/gencode/dataset/hsapiens/32/gap.bed %_ALLpos.bed
 	cut -f -3 $^ |  bedtools sort | bedtools merge > $@
 
 %.neg_rand.bed: %_onlypos.bed $(BIOINFO_ROOT)/task/gencode/dataset/hsapiens/32/chrom.info.no_alt %.neg_rand.excl.bed
