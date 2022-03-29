cdf54b40bbdb0a6b62fd049d21c2fb1609f25ee3
Chiara Cicconetti <chiara.cicconetti@unito.it>
2022-03-29 11:23:24 +0200

    v 8.7 config

--- manuscript_2023_analyses/local/share/rules/prj_rules_v8.7_ReChIRP_idr_overlap_top1000_mouse.mk
+++ manuscript_2023_analyses/local/share/rules/prj_rules_v8.7_ReChIRP_idr_overlap_top1000_mouse.mk
@@ -1,4 +1,4 @@
-ChIRP.bed.split.gz: ../../local/share/data/ReChIRP/mmusculus/overlap/overlap.conservative_peak.regionPeak.qTh005.mouse_matrix.gz
+ChIRP.bed.split.gz: ../../local/share/data/ReChIRP/mmusculus/idr_overlap_top1000/all_reproducibility.idr_conservative-idr_optimal_peak-overalp_conservative-overlap_optimal.regionPeak.top1000.gz
 	zcat $< | bedtools sort | gzip > $@
 
 %.neg_pos.bed: %_onlypos.bed %_neg.bed
@@ -7,13 +7,17 @@ ChIRP.bed.split.gz: ../../local/share/data/ReChIRP/mmusculus/overlap/overlap.con
 %_onlypos.bed: ChIRP.bed.split.gz
 	bawk '$$5=="$*" || $$5~/$*.intersec/ {print $$1,$$2,$$3,$$4";"$$5,$$5,"pos"}' $< > $@
 
+# idr + overlap top 1000 are considered positive but all the called peaks are considered in the blacklist
+%_ALLpos.bed: ../../local/share/data/ReChIRP/mmusculus/idr_overlap_top1000/all_reproducibility.idr_conservative-idr_optimal_peak-overalp_conservative-overlap_optimal.regionPeak.gz
+	bawk '$$1=="$*" {print $$2~4,"chirp_peak_" NR";"$$1,$$1,"pos"}' $< > $@
+
 %_neg.bed: ChIRP.bed.split.gz %_onlypos.bed
 	bawk '{print $$1,$$2,$$3,$$4";"$$5,"$*"}' $< > $@.tmp
 	bedtools intersect -a $@.tmp -b $^2 -v \
 	| bawk '{print $$0, "neg" }'> $@
 	rm $@.tmp
 
-%.neg_rand.excl.bed: $(GENCODE_DIR)/mm10.shuffle_blacklist.bed $(GENCODE_DIR)/gap.bed %_onlypos.bed
+%.neg_rand.excl.bed: $(GENCODE_DIR)/mm10.shuffle_blacklist.bed $(GENCODE_DIR)/gap.bed %_ALLpos.bed
 	cut -f -3 $^ |  bedtools sort | bedtools merge > $@
 
 %.neg_rand.bed: %_onlypos.bed $(GENCODE_DIR)/chrom.info.no_alt %.neg_rand.excl.bed
@@ -31,7 +35,7 @@ ALL_neg_pos.bed: $(addsuffix .neg_pos.bed,$(ALL_ssRNA))
 	cut -f -4 $^ > $@
 
 selected_ssRNA:
-	@echo 'Human lncRNAs with idr peaks number > 100'
+	@echo 'Mouse lncRNA with idr cons + idr opt + ovelap cons top 1000'
 	@echo 'See this table: https://docs.google.com/spreadsheets/d/1iQGctC1ldu1oTwmaLEsrs8cB0ik3JWYCrBkvoDKZDpk/edit#gid=0'
 
 pippo:
