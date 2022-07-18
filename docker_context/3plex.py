573dccf8c0785c370835c890b8f96dfe64c2b7af
chiaracicco <76106349+chiaracicco@users.noreply.github.com>
2022-07-18 16:43:57 +0200

    Update 3plex.py

--- docker_context/3plex.py
+++ docker_context/3plex.py
@@ -32,7 +32,7 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     epilog="Have a good triplex journey!")
 
-parser.add_argument("ssRNA", metavar="ssRNA.fa", help="The RNA sequence in fasta format. The file must contain only one sequence and the filename ([name].fa) must be the same name of the sequence in the fasta header (>[name]).")
+parser.add_argument("ssRNA", metavar="ssRNA.fa", help="The RNA sequence in fasta format. The file must contain only one sequence.")
 parser.add_argument("dsDNA", metavar="dsDNA.fa", help="The DNA seeuqnces. Multiple fasta formatted sequences, intende to be double stranded DNA potentially bound by the RNA sequence.")
 parser.add_argument("out_dir", metavar="out_dir", help="The output directory. Out_dir should already exist. Pay attention to mount appropraite volumes if you ar using docker (e.g. `docker run --rm -v $(PWD):$(PWD) 3plex:v0.0.1 ssRNA.fa dsDNA.fa $(PWD)` sould work as expected, `docker run --rm -v $(PWD):$(PWD) 3plex:v0.0.1 ssRNA.fa dsDNA.fa .` should not)")
 parser.add_argument("--bed", metavar="dsDNA.bed", dest="dsDNA_bed", type=str, default=None, help="Genomic coordiantes of the DNA sequences in bed format, the 4th column must contain the same identifiers of sequences in dsDNA.fa")
