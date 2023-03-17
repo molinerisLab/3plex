939133c930e4d2f468ddf69ba487ee1aa1b7681d
chiaracicco <chiara.cicconetti@unito.it>
2023-03-17 15:26:53 +0100

    major update of ROC.R script

--- manuscript_2023_analyses/local/src/ROC.R
+++ manuscript_2023_analyses/local/src/ROC.R
@@ -4,16 +4,16 @@ suppressMessages(library("ggplot2"))
 suppressMessages(library("pROC"))
 suppressMessages(library("optparse"))
 
+# command line usage and options
 options(error = function() traceback(2))
-
 option_list <- list(
-	make_option(c("-O", "--pdfname"), action="store", default="ROC_comparison", dest="pdf_name", help="Specify the name of the pdf image [default: \"ROC_comparison\"]"),
-        make_option(c("-d", "--direction"), action="store", default="auto", dest="direction", help="in which direction to make the comparison? “auto” (default): automatically define in which group the median is higher and take the direction accordingly. “>”: if the predictor values for the control group are higher than the values of the case group (controls > t >= cases). “<”: if the predictor values for the control group are lower or equal than the values of the case group (controls < t <= cases). You should set this explicity to “>” or “<” whenever you are resampling or randomizing the data, otherwise the curves will be biased towards higher AUC values. [default \"%default\"]")
+	make_option(c("-O", "--pdfname"), action="store", default="ROC_comparison", dest="pdf_name"
+	            , help="Specify the name of the pdf image [default: \"ROC_comparison\"]"),
+  make_option(c("-d", "--direction"), action="store", default="auto", dest="direction"
+              , help="Specify in which direction to make the comparison? “auto” (default): automatically define in which group the median is higher and take the direction accordingly. “>”: if the predictor values for the control group are higher than the values of the case group (controls > t >= cases). “<”: if the predictor values for the control group are lower or equal than the values of the case group (controls < t <= cases). You should set this explicity to “>” or “<” whenever you are resampling or randomizing the data, otherwise the curves will be biased towards higher AUC values. [default \"%default\"]"),
+	make_option(c("-s", "--smoothing"), action="store", default=NULL, dest="smoothing"
+	            , help="Method for ROC curve smoothing. Available: binormal, density, fitdistr, logcondens, logcondens.smooth. [default: no smoothing]")
 )
-
-# Esempio per passare stringa con più elementi da trasformare in un vettore di predittori
-# eset2toptable
-
 usage <- "%prog [options] outcomes_col_name [predictor_col1_name, predictor_col2_name, ...] < TABLE
 .META: stdin
 	tab separated file
@@ -24,13 +24,11 @@ usage <- "%prog [options] outcomes_col_name [predictor_col1_name, predictor_col2
 	4	AUC_2
 	5	Pvalue_cfr_1_vs_2
 "
-
-
 parser<-OptionParser(usage = usage, option_list=option_list)
 
 
-#### Assign arguments to objects
-#### Define a function to take the first element of a list and remove it from it
+# 0. Utils ----
+# Take the first element of a list and remove it
 shift_fn <- function(x) {
   if(length(x) == 0) {return(NA)}
   shiftret <- x[1]
@@ -38,60 +36,76 @@ shift_fn <- function(x) {
   return(shiftret)
 }
 
+
+# 1. Assign arguments to variables ----
 arguments <- parse_args(parser, positional_arguments = c(1,Inf))
 args <- arguments$args
 opt<-arguments$options
-
-### stdin example: "/sto1/epigen/TPXcCRE/dataset/v8_ChIRP_neg_rand/TERC-cCRE.bed.tpx.raw_-L30-e20-l10-g70-froff.summary.clean.covered_frac.stability.custom_t_pot.neg_pos_rand"
+# Read stdin 
 stdin <- file("stdin")
 open(stdin, blocking=TRUE)
-z<-read.table(stdin,header=T, sep="\t")
-
-### I take the first argument, the outcomes_col_name (the column name of the outcomes of the classification)
+z<-read.table(stdin, header=T, sep="\t")
+# Take the first argument, the outcomes_col_name (the column name of the outcomes of the classification)
 outcomes <- shift_fn(args)
-
-### The remaining arguments are the predictors
+z[,outcomes] <- as.factor(z[,outcomes])
+message("The first two levels of the \"outcomes\" variable are taken as \"control\" and \"cases\" respectively. The remaining levels are ignored.\nThe specified oredered outcomes are: ")
+message(paste0("controls: ",levels(z[,outcomes])[order(levels(z[,outcomes]))][1]))
+message(paste0("cases: ",levels(z[,outcomes])[order(levels(z[,outcomes]))][2]))
+# The remaining arguments are the predictors
 predictors <- args
+# order outcomes column
+z <- z[order(z[,outcomes]),]
+# Coerce predictor values to 4 significant digits numbers
+z[,predictors] <- sapply(z[,predictors],function(x){x <- round(as.numeric(x), digits = 4)})
+# convert infinite values if any
+z[,predictors][sapply(z[,predictors], is.infinite)] <- 1000
 
-### Definition of ROC curve formula including all predictors
-### ROC object
-roc_list <- suppressMessages(roc(as.formula(paste(collapse = "~", c(outcomes, paste(collapse = "+", predictors)))), data = z, direction=opt$direction))
 
-### Create label strings for the legend
+# 2. Define ROC obj ----
+roc_formula <- as.formula(paste(collapse = "~", c(outcomes, paste(collapse = "+", predictors))))
+roc_list <- suppressMessages(roc(roc_formula, data = z, direction = opt$direction, na.rm = T))
+
+
+# 3. ROC curve plot ----
+message(paste0("Saving ROC curves pdf plot as ", opt$pdf_name))
+# check smoothing opt
+if(!is.null(opt$smoothing)){
+  if(!opt$smoothing %in%c("binormal", "density", "fitdistr", "logcondens", "logcondens.smooth")){
+    stop("Smoothing method not available!")
+  }
+  roc_list <- lapply(roc_list, function(x){smooth(x, method=opt$smoothing)})
+}
+# Create label strings for the legend
 labels <- predictors
 for(i in seq_along(labels)){
   l<-labels[[i]]
-  auc<-round(roc_list[[l]]$auc,3)
+  auc<-round(roc_list[[l]]$auc,3) # WARNING: this line do not consider the case in which only one preditor is specified
   labels[[i]]<-paste0(c(l,auc),collapse=": ")
 }
-
-### ROC curve plot
+# plot
 ROC_plot <- ggroc(roc_list) + 
-      theme_classic(base_size = 10) +
-	    geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed") + 
-	    theme(plot.margin = margin(1,2,1,2, "cm")
-	          , plot.title = element_text(hjust = 0.5)
-	          , legend.title = element_blank()
-	          , legend.position = c(0.75, 0.22)) +
-	    scale_color_discrete(labels=labels) +
-	    scale_linetype_manual(breaks="custom_t_pot",values="dashed") +
-      coord_fixed() 
-# ggtitle("ROC Comparison") +
-
-
-### Get pdf file name from --pdf option
-### Save plot
+  geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed") +
+  theme_classic(base_size = 10) +
+  theme(plot.margin = margin(1,2,1,2, "cm")
+        , plot.title = element_text(hjust = 0.5)
+        , legend.title = element_blank()
+        , legend.position = c(0.75, 0.22)) +
+  scale_color_discrete(labels=labels) +
+  coord_fixed()
+  #scale_linetype_manual(breaks="custom_t_pot", values="dashed")
+# Save plot 
 ggsave(paste0(opt$pdf_name,".pdf"), plot=ROC_plot, device="pdf",width = 6,height=6)
 
-### Create dataframe for stdout print
-table_out <- data.frame(pred_1=character(),
-                 pred_2=character(), 
-                 AUC_1=double(), 
-                 AUC_2=double(),
-		 p_value_cfr=double(),	
-		 stringsAsFactors=FALSE) 
 
-### Fill each row of the dataframe
+# 4. Create stdout dataframe ----
+message("Saving ROC AUC comparison table as stdout")
+table_out <- data.frame(pred_1=character(),
+                        pred_2=character(),
+                        AUC_1=double(), 
+                        AUC_2=double(),
+                        p_value_cfr=double(),
+                        stringsAsFactors=FALSE) 
+# Fill each row of the dataframe
 count=1
 for(i in predictors){
   for(j in predictors){
@@ -108,16 +122,15 @@ for(i in predictors){
     } 
   }
 }
-
-### Save dataframe to stdout
-write.table(table_out, row.names =FALSE, quote = FALSE, file="", sep="\t") # col.names=NA => frist cell empty
+# Save dataframe to stdout
+write.table(table_out, row.names =FALSE, quote = FALSE, file="", sep="\t") # col.names=NA => first cell empty
       
 
-
-w=warnings()
-sink(stderr())
-if(!is.null(w)){
-        print(w)
-}
-sink()
+# sink stderr
+#w=warnings()
+#sink(stderr())
+#if(!is.null(w)){
+#  print(w)
+#}
+#sink()
 
