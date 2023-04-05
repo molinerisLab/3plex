07498e79d61e76e82faeef56ac5c9372bd355d14
chiaracicco <chiara.cicconetti@unito.it>
2023-04-06 00:28:42 +0200

    [dataset_clone_recursive] version: v_test_transcr_shuffle_h v_test_summary

---
+++ manuscript_2023_analyses/dataset/v_test_summary/rules.mk
@@ -0,0 +1,1 @@
+../../local/share/rules/rules-v_test_summary.mk
---
+++ manuscript_2023_analyses/local/share/makefiles/makefile-v_test_summary.mk
@@ -0,0 +1,9 @@
+SPECIES=hsapiens
+VERSION=32
+GENOME=hg38
+GENOME_FA=$(GENCODE_DIR)/GRCh38.primary_assembly.genome.clean_id.fa
+
+VERSION_ssRNA_FASTA=../v8.6_ReChIRP_idr_overlap_top1000
+VERSION_BED=../v8.16_top1000_human_transcript_shuffle
+
+FASTA_SHUFFLE_K=2
