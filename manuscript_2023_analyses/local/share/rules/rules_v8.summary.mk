b983de198373b7c7e23e5ce1790af10a6cbbc6cc
chiaracicco <chiara.cicconetti@unito.it>
2023-04-21 13:39:31 +0200

    update rules v8.summary

--- manuscript_2023_analyses/local/share/rules/rules_v8.summary.mk
+++ manuscript_2023_analyses/local/share/rules/rules_v8.summary.mk
@@ -20,6 +20,45 @@ CONDA_ACTIVATE=source $(CONDA_ROOT)/$(CONDA_VERSION)/etc/profile.d/conda.sh; con
 ###############
 #  ROC curves
 
+triplexAligner.matrix.human_mouse.gz: $(addsuffix /triplexAligner.matrix.gz, $(V8_DIRS))
+	r --vanilla -e 'h <- read.delim("$<",header=T);\\
+m <- read.delim("$^2",header=T);\\
+hm <- rbind(h,m[,colnames(h)]);\\
+write.table(hm,file=gzfile("$@"),quote=F,sep="\t",row.name=F,col.names=T)'
+
+fasim_LongTarget.matrix.human_mouse.gz: $(addsuffix /fasim_LongTarget.matrix.gz, $(V8_DIRS))
+	r --vanilla -e 'h <- read.delim("$<",header=T);\\
+m <- read.delim("$^2",header=T);\\
+hm <- rbind(h,m[,colnames(h)]);\\
+write.table(hm,file=gzfile("$@"),quote=F,sep="\t",row.name=F,col.names=T)'
+
+d3plex.matrix.human_mouse.gz: $(addsuffix /d3plex.matrix.gz, $(V8_DIRS))
+	r --vanilla -e 'h <- read.delim("$<",header=T);\\
+m <- read.delim("$^2",header=T);\\
+hm <- rbind(h,m[,colnames(h)]);\\
+write.table(hm,file=gzfile("$@"),quote=F,sep="\t",row.name=F,col.names=T)'
+
+%.matrix.human_mouse.auc_no_cmp.gz: %.matrix.human_mouse.gz
+	set +u; source /opt/conda/miniconda3/etc/profile.d/conda.sh; conda activate ; conda activate  pROC_Env;\
+	zcat $< | ../../local/src/ROC_no_comp.R pos_neg $$(zcat $< | head -n1 | cut -f2- | tr "\t" " ") -d "<" | gzip > $@
+
+%.matrix.human_mouse.auc_no_cmp.method_species.gz: %.matrix.human_mouse.auc_no_cmp.gz
+	bawk 'BEGIN{print"shuffling","tpx_method","auc","species"}{print $$1,"$*",$$2,"human_mouse"}' $< | gzip > $@
+
+ifeq ($(SHUFFLE_METHOD),transcript)
+d3plex.matrix.human_mouse.auc_no_cmp.method_species.gz: d3plex.matrix.human_mouse.auc_no_cmp.gz
+	bawk 'BEGIN{print "shuffling","tpx_method","auc","species"}{split($$1,a,"_"); print a[1],"d3plex_"a[2]"_"a[3]"_"a[4]"_"a[5]"_"a[6]"_"a[7]"_"a[8]"_"a[9],$$2,"human_mouse"}' $< | gzip > $@
+else
+d3plex.matrix.human_mouse.auc_no_cmp.method_species.gz: d3plex.matrix.human_mouse.auc_no_cmp.gz
+		bawk 'BEGIN{print"shuffling","tpx_method","auc","species"}{print "genomic_regions",$$1,$$2,"human_mouse"}' $< | gzip > $@
+endif
+
+
+
+
+
+
+
 tpx_paramspace_AUC_cmp.human_mouse.gz: $(addsuffix /tpx_paramspace_AUC_cmp.gz, $(V8_DIRS))
 	cat $< $^2 > $@
 
@@ -78,17 +117,15 @@ method_cmp.matrix.AUC_cmp.tsv: method_cmp.matrix.gz
 
 raw.tpx.custom_summary.neg_pos.covered_by_tts.stability.ALL_ssRNA.human_mouse.header_added.matrix.gz: raw.tpx.custom_summary.neg_pos.covered_by_tts.stability.ALL_ssRNA.human_mouse.header_added.gz selected_ssRNA.triplex_ssRNA
 	bawk 'NR>1{split($$1,a,";"); id=a[4]"_"a[6]"_"a[7]"_"a[8]"_"a[9]"_"a[2]; print a[1],$$4";"$$2,id"_Stability_best",$$13; print a[1],$$4";"$$2,id"_Stability_norm",$$18}' $< | filter_1col 1 $^2 | cut -f2- | tab2matrix -r "pos_neg;peak;ssRNA" | bawk '{split($$1,a,";"); print a[1],$$0}' | cut -f 1,3- | gzip > $@
-%.matrix.auc_no_cmp.gz: %.matrix.gz
-	set +u; source /opt/conda/miniconda3/etc/profile.d/conda.sh; conda activate ; conda activate  pROC_Env;\
-	zcat $< | ../../local/src/ROC_no_comp.R pos_neg $$(zcat $< | head -n1 | cut -f2- | tr "\t" " ") -d "<" | gzip > $@
-%.matrix.auc_no_cmp.method_species.gz: %.matrix.auc_no_cmp.gz
-	bawk 'BEGIN{print"shuffling","tpx_method","auc","species"}{print "genomic_regions",$$1,$$2,"human_mouse"}' $< | gzip > $@
 
-3plex.summary.pos_neg.ALL.human_mouse.matrix.gz: ../v8.16.1_top1000_human_transcript_shuffle/3plex.summary.pos_neg.ALL.matrix.gz ../v8.17.1_top1000_mouse_transcript_shuffle_k2/3plex.summary.pos_neg.ALL.matrix.gz 
-	r --vanilla -e 'h <- read.delim("../v8.16.1_top1000_human_transcript_shuffle/3plex.summary.pos_neg.ALL.matrix.gz",header=T);\\
-m <- read.delim("../v8.17.1_top1000_mouse_transcript_shuffle_k2/3plex.summary.pos_neg.ALL.matrix.gz",header=T);\\
+%.human_mouse.matrix.gz: ../v8.16.1_top1000_human_transcript_shuffle/%.matrix.gz ../v8.17.1_top1000_mouse_transcript_shuffle_k2/%.matrix.gz 
+	r --vanilla -e 'h <- read.delim("$<",header=T);\\
+m <- read.delim("$^2",header=T);\\
 hm <- rbind(h,m[,colnames(h)]);\\
-write.table(hm,file=gzfile("3plex.summary.pos_neg.ALL.matrix.human_mouse.gz"),quote=F,sep="\t",row.name=F,col.names=T)'
+write.table(hm,file=gzfile("$@"),quote=F,sep="\t",row.name=F,col.names=T)'
 
-3plex.summary.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.method_species.gz: 3plex.summary.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.gz
-	bawk 'BEGIN{print "shuffling","tpx_method","auc","species"}{split($$1,a,"_"); print a[1],"d3plex_"a[2]"_"a[3]"_"a[4]"_"a[5]"_"a[6]"_"a[7],$$2,"human_mouse"}' $< | gzip > $@
+triplexAligner.best_logE.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.method_species.gz: triplexAligner.best_logE.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.gz
+	bawk 'BEGIN{print "shuffling","tpx_method","auc","species"}{print $$1,"triplexAligner",$$2,"human_mouse"}' $< | gzip > $@
+fasim_LongTarget.best_meanStab.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.method_species.gz: fasim_LongTarget.best_meanStab.pos_neg.ALL.human_mouse.matrix.auc_no_cmp.gz
+	bawk 'BEGIN{print"shuffling","tpx_method","auc","species"}{print $$1,"fasimLongTarget",$$2,"human_mouse"}' $< | gzip > $@
+
