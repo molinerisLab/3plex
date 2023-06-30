077636622e70a0ba571ebf221962a783a9fab090
Ivan Molineris <ivan.molineris@unito.it>
2023-06-30 10:34:42 +0200

    simplify get_tfo_profile_range_packed

--- local/bin/tfo_profile.py
+++ local/bin/tfo_profile.py
@@ -138,10 +138,8 @@ def main():
     profiles = profile2ranges(data["profiles"])
     best_stability = best_stability_to_array(data["best_stability"], length)
     to_export = {"profiles": profiles, "best_stability": best_stability}
-    output_dir = os.path.join(path, "profile_range.msgpack")
     packed_matrix = msgpack.packb(to_export, use_bin_type=True)
-    with open(output_dir, 'wb') as file:
-        file.write(packed_matrix)
+    sys.stdout.buffer.write(packed_matrix)
 
 if __name__=="__main__":
     main()
